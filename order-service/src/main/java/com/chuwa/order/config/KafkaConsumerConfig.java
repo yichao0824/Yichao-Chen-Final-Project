e8a05ec05e4a825b6611e23ca162951576f69802
Yichao Chen <ychao0824@gmail.com>
2026-03-06 18:11:19 -0500

    Kafka works

--- order-service/src/main/java/com/chuwa/order/config/KafkaConsumerConfig.java
+++ order-service/src/main/java/com/chuwa/order/config/KafkaConsumerConfig.java
@@ -9,7 +9,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,16 +20,15 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConsumerFactory<String, PaymentEvent> consumerFactory() {
-        JsonDeserializer<PaymentEvent> deserializer = new JsonDeserializer<>(PaymentEvent.class);
+        JacksonJsonDeserializer<PaymentEvent> deserializer =
+                new JacksonJsonDeserializer<>(PaymentEvent.class);
         deserializer.addTrustedPackages("*");
-        deserializer.setUseTypeHeaders(false);
 
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "order-service-group");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
